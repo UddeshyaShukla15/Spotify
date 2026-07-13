99b0a610f08fccde6de276943b1f43a9f2de2bae
Uddeshya_Shukla <uddeshya.shukla.36.2@mountblue.tech>
2026-07-13 23:11:58 +0530

    register user done

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const userModel = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+async function registerUser (req, res){
+
+    const {username, email, password, role} = req.body;
+
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    //If user already exist than return 409 status code with message "User already exists"
+    if (isUserAlreadyExist) {
+        return res.status(409).json({
+            message: "User already exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    // If user doesn't exist, create a new user
+    const user = await userModel.create({
+        username, 
+        email, 
+        password: hash,
+        role
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+}
+
+module.exports = {registerUser};
