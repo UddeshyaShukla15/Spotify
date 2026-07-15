45870d39ec5768788f8a09d85ee29594ed11cc20
Uddeshya_Shukla <uddeshya.shukla.36.2@mountblue.tech>
2026-07-15 13:43:49 +0530

    music upload done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const authRoutes = require("./routes/auth.route.js");
+const musicRoutes = require("./routes/music.route.js");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/music", musicRoutes);
 
 module.exports = app;
